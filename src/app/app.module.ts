23def157e408279a41716fa4ee35cd4875990052
Marta Caldentey <martab.caldentey@gmail.com>
2023-05-14 16:10:18 -0300

    creacion componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ExperienciaComponent } from './components/experiencia/experiencia.component';
+import { BarranavComponent } from './components/barranav/barranav.component';
+import { HeaderComponent } from './components/header/header.component';
+import { EducacionComponent } from './components/educacion/educacion.component';
+import { SkillsComponent } from './components/skills/skills.component';
+import { ProyectosComponent } from './components/proyectos/proyectos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ExperienciaComponent,
+    BarranavComponent,
+    HeaderComponent,
+    EducacionComponent,
+    SkillsComponent,
+    ProyectosComponent
   ],
   imports: [
     BrowserModule
